9765e3abfcd81dbb4aa0a475a95daea1a76dabb9
Abayomi Osamiluyi <yomexzo@gmail.com>
2015-10-02 10:10:15 -0400

    Implemented Graded Lab #1 for Inf1340, Fall 2015 Function `vowel_or_consonant()` - tells if input is a vowel or consonant

--- lab1.py
+++ lab1.py
@@ -17,4 +17,13 @@ def vowel_or_consonant():
     "sometimes a vowel, sometimes a consonant". Otherwise your program should
     display a message indicating that the letter is a "consonant".
     """
+    alphabet = raw_input("It's time for you to tell me your alphabet")
+    if alphabet == "y":
+        print("sometimes a vowel, sometimes a consonant")
+    elif alphabet in {"a", "e", "i", "o", "u"}:
+        print("vowel")
+    else:
+        print("consonant")
+
+
 
